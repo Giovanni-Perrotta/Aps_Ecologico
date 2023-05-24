11d7d71ffeab322a3fd23d772c8ab2538c93541d
Giovanni Perrotta Paulino <60896922+Giovanni-Perrotta@users.noreply.github.com>
2023-05-24 14:32:37 -0300

    finaliza página, css e JS

--- paginas/impacto.js
+++ paginas/impacto.js
@@ -1,22 +1,3 @@
-// const imgs = document.getElementById("img")
-// const img = document.querySelectorAll("#img img")
-
-// let idx = 0;
-
-// function carrossel(){
-//     idx++;
-
-//     if(idx > img.length - 1){
-//         idx = 0;
-
-
-//     }
-
-//     img.style.transform = 'translateX(${-idx * 500}px)';
-// }
-
-// setInterval(carrossel, 1800);
-
 array1 = new Array ("../imgs/Rio1.jpg", "../imgs/Rio2.jpg", "../imgs/Rio3.jpg")
 
 function comeco(){
